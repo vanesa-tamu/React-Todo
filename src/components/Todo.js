517c1e679d350fde1150f7b1e9abb1da4153d75b
vanesa-tamu <vahe5495@tamu.edu>
2020-03-09 13:46:17 -0500

    rendering dummy data

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Todo = () => {
+const Todo = (props) => {
+    const { task } = props
     return(
         <div>
-            
+            <h1>{task.task}</h1>
         </div>
     )
 }
